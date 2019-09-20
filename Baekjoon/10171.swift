ea2cc8c4995b6e7192d6c07336c347f906caac3e
Seongmin <min_i0212@hanmail.net>
2019-09-20 18:36:09 +0900

    10171

---
+++ Baekjoon/10171.swift
@@ -0,0 +1,20 @@
+//
+//  10171.swift
+//  Baekjoon
+//
+//  Created by Hailey on 20/09/2019.
+//  Copyright © 2019 Seongmin. All rights reserved.
+//
+
+import Foundation
+
+class Num10171 {
+    static func run() {
+        print("""
+            \\    /\\
+             )  ( ')
+            (  /  )
+             \\(__)|
+            """)
+    }
+}
